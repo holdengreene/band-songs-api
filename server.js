3c1019b8b941e8baa2e564bc974bfd0d0ba802ca
Holden Greene <hgreene@cmnhospitals.org>
2018-12-18 20:23:40 -0700

    Init commit for sequelize

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(
+  "postgres://bandsongs:test@database:5432/bandsongs"
+);
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
+const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello world\n');
+app.get("/", (req, res) => {
+  res.send("Hello world\n");
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
